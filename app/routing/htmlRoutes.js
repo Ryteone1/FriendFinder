27af484516611522476c051c4b9b9b8f8b05033b
ryteone1 <rynlacy@yahoo.com>
2017-10-24 16:50:47 -0400

    yip

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,25 +1,31 @@
-// Dependencies
-// Include the path package to get the correct file path for our html. 
+// ===============================================================================
+// DEPENDENCIES
+// We need to include the path package to get the correct file path for our html
+// ===============================================================================
 var path = require("path");
 
-// var friends = require("../data/friends.js");
 
-// Routing: 
-module.exports = function(app){
-    // HTML Get Requests
+// ===============================================================================
+// ROUTING
+// ===============================================================================
 
-    // Get request for the survey page.
-    app.get("/survey", function(req, res){
-        res.sendFile(path.join(__dirname, "../public/survey.html"));
-    });
+module.exports = function(app) {
+  // HTML GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases the user is shown an HTML page of content
+  // ---------------------------------------------------------------------------
 
-    // Get request for the default/catch-all route that leads back to the home page.
-    app.get("/", function(req, res){
-        res.sendFile(path.join(__dirname, "../public/home.html"));
-    });
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/home.html"));
+  });
 
-    // Get request for the default/catch-all route that leads back to the home page.
-    app.get("/home", function(req, res){
-        res.sendFile(path.join(__dirname, "../public/home.html"));
-    });
+
+//   app.get("/reserve", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public/reserve.html"));
+//   });
+
+//   // If no matching route is found default to home
+//   app.get("*", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public/reserve.html"));
+//   });
 };
