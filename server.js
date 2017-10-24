7d7631063e24337bc1f699655a9be00e7b668ef0
ryteone1 <rynlacy@yahoo.com>
2017-10-24 00:44:12 -0400

    more

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ var characters = [
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "../public/home.html"));
+  res.sendFile(path.join(__dirname, "home.html"));
 });
 
 // app.get("/add", function(req, res) {
